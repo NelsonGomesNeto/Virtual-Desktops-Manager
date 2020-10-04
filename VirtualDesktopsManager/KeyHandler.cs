9dcb5c3f8201c1a27f9c2e6251c8a26bc7283308
Nelson Gomes Neto <ngomesneto@outlook.com>
2020-10-04 20:51:27 -0300

    Add basic Virtual Desktops Manager on WPF

---
+++ VirtualDesktopsManager/KeyHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace VirtualDesktopsManager
+{
+    public class KeyHandler
+    {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private int modifier;
+        private int key;
+        private IntPtr hWnd;
+        private int id;
+
+        public KeyHandler(int modifier, int key, IntPtr handle)
+        {
+            this.modifier = modifier;
+            this.key = (int)key;
+            this.hWnd = handle;
+            id = this.GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            return modifier ^ key ^ hWnd.ToInt32();
+        }
+
+        public bool Register()
+        {
+            return RegisterHotKey(hWnd, id, modifier, key);
+        }
+
+        public bool Unregiser()
+        {
+            return UnregisterHotKey(hWnd, id);
+        }
+    }
+
+    public static class Constants
+    {
+        // modifiers
+        public const int NOMOD = 0x0000;
+        public const int ALT = 0x0001;
+        public const int CTRL = 0x0002;
+        public const int SHIFT = 0x0004;
+        public const int WIN = 0x0008;
+
+        // windows message id for hotkey
+        public const int WM_HOTKEY_MSG_ID = 0x0312;
+
+        public const int D0 = 48; // original D0 on Windows Forms
+        public const int D1 = 49; // original D1 on Windows Forms
+    }
+}
---
+++ VirtualDesktopsManager/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WindowsDesktop;
+
+namespace VirtualDesktopsManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KeyHandler[] ghks = new KeyHandler[10];
+        private VirtualDesktop[] virtualDesktops;
+        private bool activated = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeComObjects();
+
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+
+            ComponentDispatcher.ThreadPreprocessMessage += MessageHandler;
+
+            //EnableButton.BackColor = Color.Red;
+            for (int i = 0; i < 10; i++)
+                ghks[i] = new KeyHandler(Constants.WIN + Constants.CTRL + Constants.ALT, Constants.D0 + i, handle);
+            InitializeKeyHandlers();
+
+            RefreshVirtualDesktops();
+        }
+
+        private static async void InitializeComObjects()
+        {
+            try
+            {
+                await VirtualDesktopProvider.Default.Initialize(TaskScheduler.FromCurrentSynchronizationContext());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Failed to initialize.");
+            }
+
+            VirtualDesktop.CurrentChanged += (sender, args) => System.Diagnostics.Debug.WriteLine($"Desktop changed: {args.NewDesktop.Id}");
+        }
+
+        private void RefreshVirtualDesktops()
+        {
+            virtualDesktops = VirtualDesktop.GetDesktops();
+            //virtualDesktopCountLabel.Text = virtualDesktops.Length.ToString();
+        }
+
+        private void MessageHandler(ref MSG m, ref bool handled)
+        {
+            if (m.message == Constants.WM_HOTKEY_MSG_ID)
+            {
+                int key = (((int)m.lParam >> 16) & 0xFFFF);
+                int modifier = (int)m.lParam & 0xFFFF;
+                HandleHotkey(key, modifier);
+            }
+        }
+
+        private void HandleHotkey(int key, int modifier)
+        {
+            if (key == Constants.D0)
+                virtualDesktops.Last().Switch();
+            else if (key - Constants.D1 < virtualDesktops.Length)
+                virtualDesktops[key - Constants.D1].Switch();
+        }
+
+        private void InitializeKeyHandlers()
+        {
+            for (int i = 0; i < 10; i++)
+                ghks[i].Register();
+            //EnableButton.BackColor = Color.Green;
+            //EnableButton.Text = "Disable";
+            activated = true;
+        }
+
+        private void RemoveAllKeyHandlers()
+        {
+            bool unregisterFailed = false;
+            for (int i = 0; i < ghks.Length; i++)
+                unregisterFailed |= !ghks[i].Unregiser();
+
+            if (unregisterFailed)
+                MessageBox.Show("Hotkey failed to unregister!");
+
+            //EnableButton.BackColor = Color.Red;
+            //EnableButton.Text = "Enable";
+            activated = false;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            RemoveAllKeyHandlers();
+        }
+    }
+}
